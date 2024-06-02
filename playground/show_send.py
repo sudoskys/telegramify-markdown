2e3e635a29e26fe73dfdb57467e28e4387e87614
sudoskys <coldlando@hotmail.com>
2024-06-02 11:46:44 +0800

    :recycle: fix: remove unnecessary code and update pyproject.toml

--- playground/show_send.py
+++ playground/show_send.py
@@ -7,8 +7,7 @@ import telegramify_markdown
 
 telegramify_markdown.customize.strict_markdown = False  # we need send underline text
 run_1 = telegramify_markdown.markdownify(
-    "Hello, World! HTML: &lt;strong&gt;Hello, World!&lt;/strong&gt;",
-    unescape_html=True
+    "Hello, World! HTML: &lt;strong&gt;Hello, World!&lt;/strong&gt;"
 )
 print(run_1)
 md = """
