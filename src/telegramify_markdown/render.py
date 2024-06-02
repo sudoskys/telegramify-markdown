18b94c5313022681445afa947560d1cef65bc7ec
sudoskys <coldlando@hotmail.com>
2024-06-02 11:40:07 +0800

    :recycle: fix(render): remove unused import from render.py

--- src/telegramify_markdown/render.py
+++ src/telegramify_markdown/render.py
@@ -4,7 +4,6 @@ from typing import Iterable
 
 from mistletoe import block_token, span_token
 from mistletoe.markdown_renderer import MarkdownRenderer, LinkReferenceDefinition, Fragment
-from telebot import formatting
 
 from .customize import markdown_symbol, strict_markdown
 
